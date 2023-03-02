49cbe8033dfba8aabc7768c568b79234469eb37f
yukkedon <yukkedonmusic@gmail.com>
2023-03-02 13:15:49 +0900

    lane notes json読み込み作成

--- Assets/Scripts/Main/Notes.cs
+++ Assets/Scripts/Main/Notes.cs
@@ -2,20 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainManager : MonoBehaviour
+public class Notes : MonoBehaviour
 {
-
-
-    // Start is called before the first frame update
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] float NoteSpeed = 7f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position -= transform.forward * Time.deltaTime * NoteSpeed;
     }
 }
