339a4fa432ebcdcf337cf2fae663cac284a24fa3
yukkedon <yukkedonmusic@gmail.com>
2023-03-02 14:04:35 +0900

    JudgeMessage追加

--- Assets/Scripts/Main/Notes.cs
+++ Assets/Scripts/Main/Notes.cs
@@ -6,9 +6,11 @@ public class Notes : MonoBehaviour
 {
     [SerializeField] float NoteSpeed = 7f;
 
+    bool isStart = false;
     // Update is called once per frame
     void Update()
     {
+        
         transform.position -= transform.forward * Time.deltaTime * NoteSpeed;
     }
 }
