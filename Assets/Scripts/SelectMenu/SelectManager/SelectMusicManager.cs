b6be716852a328913bffdcd80b00188a9aa102a9
hiro <n.t.hiro1@gmail.com>
2023-03-06 13:09:52 +0900

    2023/03/06 TMPro用のフォント追加、セレクト画面仮実装

---
+++ Assets/Scripts/SelectMenu/SelectManager/SelectMusicManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+
+/// <summary>
+/// 曲選択画面用のマネージャー
+/// </summary>
+public class SelectMusicManager : MonoBehaviour
+{
+   
+}
