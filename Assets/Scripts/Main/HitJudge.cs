18ca38a39861958a36e8a061b69c92885f1add9a
yukkedon <yukkedonmusic@gmail.com>
2023-03-13 13:34:31 +0900

    ロングノーツの構造を改良

--- Assets/Scripts/Main/HitJudge.cs
+++ Assets/Scripts/Main/HitJudge.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class HitJudge : MonoBehaviour
 {
+
 
     [SerializeField] MainManager mainManager;
     [SerializeField] GameObject[] JudgeMsgObj;
@@ -22,11 +24,23 @@ public class HitJudge : MonoBehaviour
     [SerializeField] float BadSecond    = 0.20f;
     [SerializeField] float MissSecond   = 0.20f;
 
+    enum PusingKey
+    {
+        D,F,J,K,
+    }
+
+    bool[] pushingKeyState = new bool[4] { false, false, false, false };
+
     // Update is called once per frame
     void Update()
     {
+        // ロングノーツがあるかどうか
+        bool isLong0 = notesManager.NoteDataAll[0].type == 2;
+        bool isLong1 = notesManager.NoteDataAll[1].type == 2;
+
         if (mainManager.isStart && !mainManager.isEnd)
         {
+
             // 各レーンに対応したボタン入力の処理
             if (Input.GetKeyDown(KeyCode.D))
             {
@@ -68,7 +82,7 @@ public class HitJudge : MonoBehaviour
                 {
                     if (notesManager.NoteDataAll[1].laneNum == 2)
                     {
-                        CheckHitTiming(Mathf.Abs(Time.time - (notesManager.NoteDataAll[1].time + mainManager.startTime)), 1);
+                        CheckHitTiming(Mathf.Abs(Time.time - (notesManager.NoteDataAll[1].time + mainManager.startTime)), 1, isLong1);
                     }
                 }
                 soundMain.PlaySE((int)SoundMain.SE.Touch);
@@ -77,18 +91,20 @@ public class HitJudge : MonoBehaviour
             {
                 if (notesManager.NoteDataAll[0].laneNum == 3)
                 {
-                    CheckHitTiming(Mathf.Abs(Time.time - (notesManager.NoteDataAll[0].time + mainManager.startTime)),0);
+                    CheckHitTiming(Mathf.Abs(Time.time - (notesManager.NoteDataAll[0].time + mainManager.startTime)),0, isLong0);
                     mainManager.ResetCombo();
                 }
                 else
                 {
                     if (notesManager.NoteDataAll[1].laneNum == 3)
                     {
-                        CheckHitTiming(Mathf.Abs(Time.time - (notesManager.NoteDataAll[1].time + mainManager.startTime)), 1);
+                        CheckHitTiming(Mathf.Abs(Time.time - (notesManager.NoteDataAll[1].time + mainManager.startTime)), 1, isLong1);
                     }
                 }
                 soundMain.PlaySE((int)SoundMain.SE.Touch);
             }
+
+            
 
             // ミス判定
             if (Time.time > notesManager.NoteDataAll[0].time + MissSecond + mainManager.startTime)
@@ -99,6 +115,16 @@ public class HitJudge : MonoBehaviour
                 mainManager.AddJudgeCount(3);
             }
         }
+        UpdatePushingKeyState();
+
+        if (isLong0)
+        {
+            UpdateLongNotes(notesManager.NoteDataAll[0]);
+        }
+        else if (isLong1)
+        {
+            UpdateLongNotes(notesManager.NoteDataAll[1]);
+        }
 
     }
 
@@ -107,8 +133,9 @@ public class HitJudge : MonoBehaviour
         UpdateText();
     }
 
-    void CheckHitTiming(float timeLag,int offset)
+    void CheckHitTiming(float timeLag,int offset,bool isLong = false)
     {
+        
         if (timeLag <= PerfectSecond)
         {
             PopupJudgeMsg(0,offset);
@@ -130,12 +157,54 @@ public class HitJudge : MonoBehaviour
             mainManager.ResetCombo();
             mainManager.AddJudgeCount(2);
         }
+
+    }
+
+    void UpdateLongNotes(NoteData notesData)
+    {
+
+    }
+
+    
+    void UpdatePushingKeyState()
+    {
+        pushingKeyState[(int)PusingKey.D] = Input.GetKey(KeyCode.D);
+        pushingKeyState[(int)PusingKey.F] = Input.GetKey(KeyCode.F);
+        pushingKeyState[(int)PusingKey.J] = Input.GetKey(KeyCode.J);
+        pushingKeyState[(int)PusingKey.K] = Input.GetKey(KeyCode.K);
     }
 
     void DeleteData(int offset)
     {
+        if (notesManager.NoteDataAll[offset].longNotes != null)
+        {
+            foreach(LongNoteData notes in notesManager.NoteDataAll[offset].longNotes)
+            {
+                Debug.Log(notes.ToString());
+                Destroy(notes.notes);
+            }
+        }
+
         notesManager.NoteDataAll.RemoveAt(offset);
         Destroy(notesManager.NotesObj[offset]);
+        notesManager.NotesObj.RemoveAt(offset);
+
+        if (notesManager.NoteDataAll.Count <= 0)
+        {
+            mainManager.isEnd = true;
+        }
+    }
+
+    // ロングノーツの情報を持っているオブジェクト・リストの削除
+    void DeleteDataLongNotes(int offset)
+    {
+        
+        Destroy(notesManager.NotesObj[offset]);
+        foreach(LongNoteData longnotes in notesManager.NoteDataAll[offset].longNotes)
+        {
+            Destroy(notesManager.NotesObj[offset]);
+        }
+
         notesManager.NotesObj.RemoveAt(offset);
 
         if (notesManager.NoteDataAll.Count <= 0)
@@ -155,6 +224,14 @@ public class HitJudge : MonoBehaviour
         Instantiate(JudgeMsgObj[judge], new Vector3(notesManager.NoteDataAll[offset].laneNum - 1.5f, 0.76f, 0.15f), Quaternion.Euler(45, 0, 0));
     }
 
+    bool PushingKey()
+    {
+        if (Input.anyKey)
+        {
+            return true;
+        }
+        return false;
+    }
     void AddPoint()
     {
         mainManager.point = (int)Math.Round(1000000 * Math.Floor(mainManager.playerScore / mainManager.maxScore * 1000000) / 1000000);
--- Assets/Scripts/Main/NotesManager.cs
+++ Assets/Scripts/Main/NotesManager.cs
@@ -16,25 +16,37 @@ public class NoteData
     public int laneNum;
     public float LPB;
     public List<LongNoteData> longNotes;
-    public NoteData() 
-    {
-        longNotes = new List<LongNoteData>();
-    }
+
+
     public NoteData(int type,float time,int laneNum,float LPB) 
     { 
         this.type = type;
         this.time = time;
         this.laneNum = laneNum;
         this.LPB = LPB;
+        if (this.type == 2)
+        {
+            longNotes = new List<LongNoteData>();
+        }
     }
 }
 
 public class LongNoteData
 {
     public int type;
-    public float num;
-    public int block;
-    public int LPB;
+    public float time;
+    public int laneNum;
+    public float LPB;
+
+    public LongNoteData(int type, float time, int laneNum, float LPB)
+    {
+        this.type = type;
+        this.time = time;
+        this.laneNum = laneNum;
+        this.LPB = LPB;
+    }
+
+    public GameObject notes;
 }
 
 enum NotesType
@@ -49,15 +61,11 @@ public class NotesManager : MonoBehaviour
     public int noteNum;
 
     [SerializeField] MainManager mainManager;
+    [SerializeField] Material NotesLineMaterial;
 
     public List<NoteData> NoteDataAll = new List<NoteData>();
     public List<GameObject> NotesObj = new List<GameObject>();
 
-    /*public List<int> LaneNum = new List<int>();
-    public List<int> NoteType = new List<int>();
-    public List<float> NotesTime = new List<float>();
-    
-    public List<GameObject> LongNotesObj = new List<GameObject>();*/
 
     [SerializeField] float NotesSpeed;
     [SerializeField] GameObject noteObj;
@@ -87,6 +95,8 @@ public class NotesManager : MonoBehaviour
 
         for (int i = 0; i < jsonData["notes"].Count; i++)
         {
+            // JsonDataクラスで(flaot)(int)キャストするとエラーになるため
+            // 一時的に保存する変数を作成
             string LPB = jsonData["notes"][i]["LPB"].ToString();
             string NUM = jsonData["notes"][i]["num"].ToString();
             string BLOCK = jsonData["notes"][i]["block"].ToString();
@@ -102,15 +112,12 @@ public class NotesManager : MonoBehaviour
             NotesObj.Add(Instantiate(noteObj, new Vector3(noteData.laneNum - 1.5f, 0.55f, z), Quaternion.identity));
             NoteDataAll.Add(noteData);
 
-            Debug.Log(i+":"+noteData.time);
-
             // ロングノーツ判定
             if (noteData.type == 2)
             {
                 // ロングノーツ作成処理
                 for (int j = 0; j < jsonData["notes"][i]["notes"].Count; j++){
-                    // JsonDataクラスで(flaot)(int)キャストするとエラーになるため
-                    // 一時的に保存する変数を作成
+
                     LPB      = jsonData["notes"][i]["notes"][j]["LPB"].ToString();
                     NUM      = jsonData["notes"][i]["notes"][j]["num"].ToString();
                     BLOCK    = jsonData["notes"][i]["notes"][j]["block"].ToString();
@@ -125,22 +132,35 @@ public class NotesManager : MonoBehaviour
                     z = noteData.time * NotesSpeed;
                     
                     noteNum++;
+
+                    LongNoteData longnoteData = new LongNoteData(int.Parse(TYPE), time, int.Parse(BLOCK), float.Parse(LPB));
+                    longnoteData.notes = (GameObject)Instantiate(noteObj, new Vector3(noteData.laneNum - 1.5f, 0.55f, z), Quaternion.identity);
+
+                    NoteDataAll[NoteDataAll.Count - 1].longNotes.Add(longnoteData);
+
+                    // 最初に軌道する場合はこちら
+                    if (NoteDataAll[NoteDataAll.Count - 1].longNotes.Count == 1)
+                    {
+                        LongNotesCreate(NotesObj[NotesObj.Count - 1].transform, longnoteData.notes.transform, NotesObj[NotesObj.Count - 1]);
+                    }
+                    else
+                    {
+                        // 2個以上は1つ前のノーツ情報を取得するため j - 1 で指定している
+                        LongNotesCreate(NoteDataAll[NoteDataAll.Count - 1].longNotes[j - 1].notes.transform, longnoteData.notes.transform, NotesObj[NotesObj.Count - 1]);
+                    }
                     
-                    NotesObj.Add(Instantiate(noteObj, new Vector3(noteData.laneNum - 1.5f, 0.55f, z), Quaternion.identity));
 
-                    LongNotesCreate(NotesObj[NotesObj.Count - 2].transform,NotesObj[NotesObj.Count - 1].transform);
-
-                    NoteDataAll.Add(noteData);
+                    //NoteDataAll.Add(noteData);
                 }
                 
             }
         }
-        Debug.Log(noteNum);
         mainManager.maxScore = noteNum * mainManager.MAX_RAITO_POINT;
     }
 
-    private const int LANE_WIDTH = 1;
-    private void LongNotesCreate(Transform start, Transform end)
+    private const float LANE_WIDTH = 1.0f;
+    private const float NOTES_SIZE_OFFSET = 0.05f;  // ロングノーツの軌道サイズを調整
+    private void LongNotesCreate(Transform start, Transform end,GameObject notes)
     {
 
 
@@ -150,17 +170,24 @@ public class NotesManager : MonoBehaviour
 
         Mesh mesh = new Mesh();
         longNotesLine.GetComponent<MeshFilter>().mesh = mesh;
+        
         Vector3[] vertices = new Vector3[4];
-        int[] triangles = { 0, 2, 1, 3, 1, 2 };
+        int[] triangles = new int[6];
 
-        vertices[0] = start.position + new Vector3(-LANE_WIDTH / 2, 0, 0);
-        vertices[1] = start.position + new Vector3( LANE_WIDTH / 2, 0, 0);
-        vertices[2] = end.position   + new Vector3( LANE_WIDTH / 2, 0, 0);
-        vertices[3] = end.position   + new Vector3(-LANE_WIDTH / 2, 0, 0);
+        vertices[0] = start.position + new Vector3(-LANE_WIDTH / 2.0f + NOTES_SIZE_OFFSET, 0, 0);
+        vertices[1] = start.position + new Vector3( LANE_WIDTH / 2.0f - NOTES_SIZE_OFFSET, 0, 0);
+        vertices[2] = end.position   + new Vector3( LANE_WIDTH / 2.0f - NOTES_SIZE_OFFSET, 0, 0);
+        vertices[3] = end.position   + new Vector3(-LANE_WIDTH / 2.0f + NOTES_SIZE_OFFSET, 0, 0);
+
+        triangles = new int[6] { 0,2,1,3,2,0};
 
         mesh.vertices  = vertices;
         mesh.triangles = triangles;
 
+        longNotesLine.GetComponent<Renderer>().material = NotesLineMaterial;
+
         mesh.RecalculateNormals();
+
+        longNotesLine.transform.parent = notes.transform;
     }
 }
