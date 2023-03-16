d7c2e580d55a8a9ecf7fe7c24e56c71de2fd55f6
hiro10 <n.t.hiro1@gmail.com>
2023-03-16 15:01:23 +0900

    セレクト画面実装途中（バグ付き）

--- Assets/Scripts/SelectMenu/Scroll/SelectViewer.cs
+++ Assets/Scripts/SelectMenu/Scroll/SelectViewer.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+using LitJson;
 
 /// <summary>
 /// ScrollViewに実際に表示させるデータを渡したりする。
@@ -9,12 +13,40 @@ using System.Linq;
 /// </summary>
 public class SelectViewer : BaseSound
 {
+    public List<string> jsonKey = new List<string>();
+
     // スクロールパネル
     [SerializeField] private ScrollView scrollview;
 
+    [SerializeField] SoundSelect soundSelect;
+
     private void Awake()
     {
-        LoadBGM("", true);
+        Load();
+    }
+    /// <summary>
+    /// jsonロード
+    /// </summary>
+    private void Load()
+    {
+        AsyncOperationHandle json;
+
+        // jsonをaddressableで取得
+        json = Addressables.LoadAssetAsync<TextAsset>($"Assets/Resource/CutMusic/NameList.json");
+
+        // 同期処理化
+        var scoreLoad = json.WaitForCompletion();
+
+        TextAsset score = json.Result as TextAsset;
+        JsonData jsonData = JsonMapper.ToObject(score.ToString());
+
+        Addressables.Release(json);
+
+        for (int i = 0; i < jsonData.Count; i++)
+        {
+            jsonKey.Add(jsonData[i].ToString());
+            Debug.Log(jsonKey[i]);
+        }
     }
 
     /// <summary>
@@ -23,6 +55,7 @@ public class SelectViewer : BaseSound
     private void Start()
     {
         StartCoroutine(StartScrollObj());
+        LoadBGM("",true);
     }
 
     private void Update()
@@ -33,8 +66,13 @@ public class SelectViewer : BaseSound
     IEnumerator StartScrollObj()
     {
         yield return new WaitForSeconds(2f);
-        var items = Enumerable.Range(0, GetbgmClip().Count).
-          Select(i => new MusicItemData(i, $""+ GetBgmClipName(i))).ToArray();
+        var items = Enumerable.Range(0, jsonKey.Count).
+          Select(i => new MusicItemData(jsonKey.Count, jsonKey[i])).ToArray();
         scrollview.UpdateData(items);
+    }
+
+   public void PlayBgm()
+    {
+        soundSelect.PlayBGM(0);
     }
 }
